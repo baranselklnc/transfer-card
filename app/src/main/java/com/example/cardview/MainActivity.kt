8abe10dbdec91ff5b35cc7e120b6ce022ac1a884
Baransel <baranselklnc@gmail.com>
2023-09-03 01:47:49 +0300

    initialized

---
+++ app/src/main/java/com/example/cardview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.cardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //https://github.com/baranselklnc/fakejson/blob/master/fakeapiFootball.json
+
+
+    }
+
+
+}
