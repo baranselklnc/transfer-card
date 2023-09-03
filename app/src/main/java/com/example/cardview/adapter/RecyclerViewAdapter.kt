34b2c9ab1e54fd1ad1aa53eafff52dbfb11eeb80
Baransel <baranselklnc@gmail.com>
2023-09-04 02:58:02 +0300

    The data retrieved from the API has been displayed in CardView through the RecyclerViewAdapter.

---
+++ app/src/main/java/com/example/cardview/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.cardview.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.cardview.R
+import com.example.cardview.model.FootballModel
+
+class RecyclerViewAdapter (private val footbaList:ArrayList<FootballModel>):RecyclerView.Adapter<RecyclerViewAdapter.RowHolder>(){
+
+    class RowHolder(view:View):RecyclerView.ViewHolder(view){
+
+        fun bind(footballModel: FootballModel){
+          val textView:TextView=itemView.findViewById(R.id.textPlayer)
+          val textView2:TextView=itemView.findViewById(R.id.textTeam)
+            val imageView:ImageView=itemView.findViewById(R.id.playerPhoto)
+            textView.text=footballModel.name
+            textView2.text=footballModel.team
+            val imageUrl=footballModel.photo_url
+            Glide.with(itemView.context).load(imageUrl).into(imageView)
+
+
+
+        }
+
+
+    }
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RowHolder {
+        val view =LayoutInflater.from(parent.context).inflate(R.layout.cardview,parent,false)
+        return RowHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: RowHolder, position: Int) {
+          holder.bind(footbaList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return footbaList.count()
+    }
+
+
+}
