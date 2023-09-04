2a23e0b50b90ec29e72419501e1576df65059ab4
Baransel <baranselklnc@gmail.com>
2023-09-05 02:39:56 +0300

    Call has been deprecated

--- app/src/main/java/com/example/cardview/service/FootballAPI.kt
+++ app/src/main/java/com/example/cardview/service/FootballAPI.kt
@@ -1,11 +1,12 @@
 package com.example.cardview.service
 
 import com.example.cardview.model.FootballModel
+import io.reactivex.rxjava3.core.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface FootballAPI {
     @GET("baranselklnc/fakejson/master/fakeapiFootball.json")
-
-    fun getData():Call<List<FootballModel>>
+   fun getData():Observable<List<FootballModel>>
+   // fun getData():Call<List<FootballModel>>
 }
