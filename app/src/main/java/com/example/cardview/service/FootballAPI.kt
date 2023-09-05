0de1dd6040b3af51b84f25145c0d40424beff29b
Baransel <baranselklnc@gmail.com>
2023-09-05 03:18:25 +0300

    "In the latest update, we have transitioned away from using Call and have now adopted the usage of Observable."

--- app/src/main/java/com/example/cardview/service/FootballAPI.kt
+++ app/src/main/java/com/example/cardview/service/FootballAPI.kt
@@ -1,12 +1,12 @@
 package com.example.cardview.service
 
 import com.example.cardview.model.FootballModel
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface FootballAPI {
     @GET("baranselklnc/fakejson/master/fakeapiFootball.json")
-   fun getData():Observable<List<FootballModel>>
+   fun getData(): Observable<List<FootballModel>>
    // fun getData():Call<List<FootballModel>>
 }
